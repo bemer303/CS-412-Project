1baddc503a5d0a973cb0bf5304f0a0374364036a
Bryan Emer <bryanemer@gmail.com>
2021-12-01 23:32:49 -0600

    cross-validation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB, BernoulliNB, CategoricalNB, ComplementNB
 import sklearn.preprocessing as pp
 from sklearn import metrics
@@ -56,18 +57,32 @@ if __name__ == '__main__':
     bnb.fit(X, y)
     clf.fit(X, y)
 
+    model = LogisticRegression(solver='lbfgs', max_iter=10000)
+    model.fit(X, y)
+    y_pred5 = model.predict(X_test)
+
+
     y_pred = gnb.predict(X_test)
     y_pred2 = bnb.predict(X_test)
     y_pred3 = clf.predict(X_test)
     y_pred4 = neigh.predict(X_test)
     # cross validation
-    scores = cross_val_score(gnb, X, y, cv=5)
-    print(scores)
+    gnb_scores = cross_val_score(gnb, X, y, cv=5) * 100
+    bnb_scores = cross_val_score(bnb, X, y, cv=5) * 100
+    clf_scores = cross_val_score(clf, X, y, cv=5) * 100
+    neigh_scores = cross_val_score(neigh, X, y, cv=5) * 100
+    log_scores = cross_val_score(model, X, y, cv=5) * 100
     # accuracy tests
     print("Gaussian Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred)*100)
+    print("Gaussian Naive Bayes cross validation(in %):", gnb_scores)
     print("Bernoulli Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred2) * 100)
+    print("Bernoulli Naive Bayes cross validation(in %):", bnb_scores)
     print("Complement Naive Bayes model accuracy(in %):", metrics.accuracy_score(y_test, y_pred3) * 100)
+    print("Complement Naive Bayes cross validation(in %):", clf_scores)
     print("KNN model accuracy(in %):", metrics.accuracy_score(y_test, y_pred4) * 100)
+    print("KNN cross validation(in %):", neigh_scores)
+    print("Logistic Regression model accuracy(in %):", metrics.accuracy_score(y_test, y_pred5) * 100)
+    print("Logistic Regression cross validation(in %):", log_scores)
     
 
 
